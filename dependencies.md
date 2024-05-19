f16c8052a8421f24f58d2e045606b808829a2cb4
felix920506 <felix920506@gmail.com>
2024-05-19 10:45:39 +0800

    Add Styling

---
+++ dependencies.md
@@ -0,0 +1,1 @@
+https://github.com/UN-GCPDS/qt-material?tab=BSD-2-Clause-1-ov-file#readme
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget, QLabel
+
+# Custom Widgets
 from placeholderwidget import PlaceholderWidget
 from loginform import loginForm
+
+# Other things
 import sessioncontroller
+import qt_material
 
 app = QApplication(sys.argv)
 
 window = QMainWindow()
+
+# Add Widgets
 
 mainWidget = QStackedWidget()
 mainWidget.addWidget(loginForm())
@@ -18,6 +25,9 @@ if sessioncontroller.validateCurrentSession():
     mainWidget.setCurrentIndex(1)
 
 window.setCentralWidget(mainWidget)
-window.show()
 
+# TODO: Make theme a setting
+qt_material.apply_stylesheet(app, theme='./themes/dark_amber.xml')
+
+window.show()
 app.exec()
