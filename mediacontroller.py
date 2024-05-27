c1f6d684425d79e98722a57c9895e5b712784041
felix920506 <felix920506@gmail.com>
2024-05-27 11:09:06 +0800

    Add media controller

---
+++ mediacontroller.py
@@ -0,0 +1,22 @@
+import requests
+import sessioncontroller
+
+
+def getLatest() -> list:
+    pass
+
+
+def getLibs() -> list:
+    pass
+
+
+def getLibAlbums(id: str) -> list:
+    res = sessioncontroller.get('UserViews')
+
+
+def getAlbumInfo(id: str) -> dict:
+    pass
+
+
+def getTrackInfo(id: str) -> dict:
+    pass
